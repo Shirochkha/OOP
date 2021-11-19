68b8662d218b64665abda65b622426dfab213fdf
Olga Ovchinina <chess.rs56@gmail.com>
2021-11-19 08:33:06 +0300

    Лабораторная работа№3

---
+++ lab3.2/lab3.2/Array1D.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Arrays
+{
+    public class Array1D
+    {
+
+        private float[] floatArray;
+
+        //длина массива
+        public int Length
+        {
+            get { return floatArray.Length; }
+        }
+
+        //индексатор
+        public float this[int index]
+        {
+            get { return floatArray[index]; }
+            set { floatArray[index] = value; }
+        }
+
+        //конструктор по умолчанию
+        public Array1D()
+        {
+            floatArray = new float[5];
+        }
+
+        //конструктор с парметрами
+        public Array1D(int length)
+        {
+            floatArray = new float[length];
+        }
+
+        // Метод, позволяющий ввести элементы массива с клавиатуры.
+        //parse - считывает введеную строку и преобразует в вещественное число.
+        public void InputFloatArray()
+        {
+            Console.WriteLine("Введите элименты массива: ");
+            for (int i = 0; i < floatArray.Length; i++)
+            {
+                Console.Write("A[{0}] = ", i);
+                floatArray[i] = float.Parse(Console.ReadLine());
+            }
+        }
+
+        // Метод, позволяющий вывести элементы массива на экран.
+        public void ShowFloatArray()
+        {
+            foreach (float item in floatArray)
+            {
+                Console.Write("{0} ", item);
+            }
+            Console.WriteLine();
+        }
+
+        //Количество отрицательных элементов во всем массиве.
+        public int Count()
+        {
+            int minus = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if (this[i] < 0)
+                {
+                    minus++;
+                }
+            }
+            return minus;
+        }
+
+        //Количество отрицательных элементов после элемента с заданным номером.
+        public int Count(int num)
+        {
+            int minus = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if ((i > num) && (this[i] < 0))
+                {
+                    minus++;
+                }
+            }
+            return minus;
+        }
+
+        //Количество отрицательных элементов, больших заданного
+        public int Count(float number)
+        {
+            int minus = 0;
+            for (int i = 0; i < Length; i++)
+            {
+                if ((this[i] > number) && (this[i] < 0))
+                {
+                    minus++;
+                }
+            }
+            return minus;
+        }
+
+        //Умножение массивов.
+        public static Array1D operator *(Array1D a, Array1D b)
+        {
+            Array1D newArray = new Array1D(Math.Max(a.Length, b.Length));
+
+            if (a.Length > b.Length)
+            {
+                for (int i = 0; i < b.Length; i++)
+                {
+                    newArray[i] = a[i] * b[i];
+                }
+
+                for (int i = b.Length; i < a.Length; i++)
+                {
+                    newArray[i] = a[i];
+                }
+            }
+            else
+            {
+                for (int i = 0; i < a.Length; i++)
+                {
+                    newArray[i] = a[i] * b[i];
+                }
+
+                for (int i = a.Length; i < b.Length; i++)
+                {
+                    newArray[i] = b[i];
+                }
+            }
+
+            return newArray;
+        }
+
+        //Операция неявного преобразования целого числа в объект класса.
+        public static implicit operator Array1D(int x)
+        {
+            Array1D newArray = new Array1D(x);
+
+            for (int i = 0; i < x; i++)
+            {
+                newArray[i] = 1;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ lab3.2/lab3.2/Menu.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    /// <summary>
+    /// Show Menu.
+    /// </summary>
+    class Menu
+    {
+        public static void ShowMenu()
+        {
+            Console.WriteLine("Введите размерность массива A");
+            Array1D arrayA = new Array1D(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("\tМассив A");
+            arrayA.InputFloatArray();
+
+            Console.WriteLine("Введите размерность массива B");
+            Array1D arrayB = new Array1D(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("\tМассив B");
+            arrayB.InputFloatArray();
+
+            Console.WriteLine("Введите размерность массива C");
+            Array1D arrayC = new Array1D(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("\tМассив C");
+            arrayC.InputFloatArray();
+
+            try
+            {
+                int flag = 0;
+
+                while (flag != 4)
+                {
+                    Console.Write("Массив A: ");
+                    arrayA.ShowFloatArray();
+                    Console.Write("Массив B: ");
+                    arrayB.ShowFloatArray();
+                    Console.Write("Массив C: ");
+                    arrayC.ShowFloatArray();
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("\n\t╔═══════════════════════════════════════════════════════════╗");
+                    Console.WriteLine("\t║                             МЕНЮ                          ║");
+                    Console.WriteLine("\t╟─────┬─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  1  │   Количество отрицательных элементов массива A*C    ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  2  │  Количество отрицательных элементов массивов A и C  ║");
+                    Console.WriteLine("\t║     │           после элемента с заданным номером         ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║     │   Если в массиве A*B есть отрицательные элементы,   ║");
+                    Console.WriteLine("\t║     │   превышающие -5.3, сформировать массив из 500      ║");
+                    Console.WriteLine("\t║  3  │   элементов, в котором все элементы с номерами,     ║");
+                    Console.WriteLine("\t║     │   кратными 10, равны 0, а остальные равны 1         ║");
+                    Console.WriteLine("\t╟─────┼─────────────────────────────────────────────────────╢");
+                    Console.WriteLine("\t║  4  │                       Выход                         ║");
+                    Console.WriteLine("\t╚═══════════════════════════════════════════════════════════╝");
+
+                    flag = Convert.ToInt32(Console.ReadLine());
+
+                    switch (flag)
+                    {
+                        case 1:
+                            Console.WriteLine("\tРезультат: {0}", (arrayA*arrayC).Count());
+                            break;
+                        case 2:
+                            Console.WriteLine("\tВведите номер элемента");
+                            int num = Convert.ToInt32(Console.ReadLine());
+                            if ((num < arrayA.Length) && (num < arrayC.Length))
+                            {
+                                Console.WriteLine("\tРезультат: {0} и {1}", arrayA.Count(num), arrayC.Count(num));
+                            }
+                            else
+                            {
+                                Console.WriteLine("\tЭлемент превышает размерность массива");
+                            }
+                            break;
+                        case 3:
+                            if((arrayA * arrayB).Count((float)-5.3) > 0)
+                            {
+                                Array1D arrayD = 500;
+                                for(int i=0; i<500; i++)
+                                {
+                                    if(i%10 == 0)
+                                    {
+                                        arrayD[i] = 0;
+                                    }
+                                }
+                                Console.Write("\tРезультат : ");
+                                arrayD.ShowFloatArray();
+                            }
+                            else
+                            {
+                                Console.WriteLine("\tВ массиве A*B нет отрицательных элементов, превышающих -5.3");
+                                (arrayA* arrayB).ShowFloatArray();
+                            }    
+                            break;
+                        case 4:
+                            Console.WriteLine("\tПрограмма завершена");
+                            break;
+                        default:
+                            Console.WriteLine("\tТакого пункта меню нет");
+                            break;
+                    }
+                    Console.WriteLine("\tНажмите любую клавишу для продолжения...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
